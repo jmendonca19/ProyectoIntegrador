e69ff244db6f522e57588b0795bf03639f728d24
ivanfischman <ivanfischman@gmail.com>
2022-04-06 14:13:09 -0300

    db comentarios

--- db/data.js
+++ db/data.js
@@ -8,7 +8,34 @@ const data = {
             dni: 44517526 ,
             fotoPerfil: "" ,
         },
-]
+    ],
+    comentarios: [
+        {
+           usuario: "Ivan Fischman",
+           comentario: "Hola! como me puedo contactar con vos? tengo muchas preguntas, me gusto el auto. Gracias",
+           fotoUsuarios: "" 
+        },
+        {
+           usuario: "Juana Mendonca",
+           comentario: "Aceptas efectivo? si es asi me lo dejas mas barato?",
+           fotoUsuarios: "" 
+        },
+        {
+           usuario: "Carlitos Perez",
+           comentario: "Hola, me interesa. como hacemos?",
+           fotoUsuarios: "" 
+        },
+        {
+           usuario: "Alan Gomez",
+           comentario: "Tiene alguna marca? cuantos kilometros tiene?",
+           fotoUsuarios: "" 
+        },
+        {
+           usuario: "Maria Vecerra",
+           comentario: "No me gusta el auto, muy feo!",
+           fotoUsuarios: "" 
+        }
+    ]
 }
 
 module.exports = data;
