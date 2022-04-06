e3b1ebe8b8cc90d6fdd09adbd828b2dfb26984d0
jmendonca19 <jmendonca@udesa.edu.ar>
2022-04-06 14:18:34 -0300

    base de datos productos

--- db/data.js
+++ db/data.js
@@ -8,7 +8,71 @@ const data = {
             dni: 44517526 ,
             fotoPerfil: "" ,
         },
-]
+],
+    productos: [
+        {
+            nombreProducto: "volkswagen modelo 2016",
+            descripcion: "scirocco",
+            fechaCarga: "23/3/2020",
+            imagen: "black",
+        },
+        {
+            nombreProducto: "volkswagen modelo 2020",
+            descripcion: "scirocco",
+            fechaCarga: "19/02/2021",
+            imagen: "red",
+        },
+        {
+            nombreProducto: "volkswagen modelo 2015",
+            descripcion: "scirocco",
+            fechaCarga: "25/02/2021",
+            imagen: "white", 
+        },
+        {
+            nombreProducto: "chevrolet modelo 2016",
+            descripcion: "cruze",
+            fechaCarga: "05/07/2017",
+            imagen: "white",
+        },
+        {
+            nombreProducto: "nissan modelo 2020",
+            descripcion: "sentra",
+            fechaCarga: "12/03/2022",
+            imagen: "gray",
+        },
+        {
+            nombreProducto: "toyota modelo 2022",
+            descripcion: "etios",
+            fechaCarga: "06/02/2022",
+            imagen: "black",
+        },
+        {
+            nombreProducto: "audi modelo 2019",
+            descripcion: "a6",
+            fechaCarga: "03/04/2020",
+            imagen: "black",
+        },
+        {
+            nombreProducto: "toyota modelo 2014",
+            descripcion: "Corolla",
+            fechaCarga: "20/09/2015",
+            imagen: "blue",
+        },
+        {
+            nombreProducto: "peugeot modelo 2010",
+            descripcion: "208",
+            fechaCarga: "09/08/2011",
+            imagen: "white",
+        },
+        {
+            nombreProducto: "audi modelo 2022",
+            descripcion: "tt",
+            fechaCarga: "10/03/2022",
+            imagen: "black",
+        },
+    ]
 }
+
+
 
 module.exports = data;
