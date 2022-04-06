8467c7b86194ea60c88af5d196e3e67edc5320ba
Joaquin Knez <joaquinknez@MBPdeJoaquin2.fibertel.com.ar>
2022-04-06 13:56:35 -0300

    base de datos

---
+++ db/data.js
@@ -0,0 +1,14 @@
+const data = {
+    usuario:[
+        {
+            email: "joaknezaustral@gmail.com",
+            usuario: "Joaquin Knez",
+            contrasenia: "Holasoyjoaquin",
+            fechaNacimiento: "09/04/2003" ,
+            dni: 44517526 ,
+            fotoPerfil: "" ,
+        },
+]
+}
+
+module.exports = data;
